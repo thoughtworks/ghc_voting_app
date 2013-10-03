7685cb91e1e2ae5567113534210f5b2f8d17d397
Emily Rosengren <erosengr@thoughtworks.com>
2013-10-03 12:29:45 -0500

    fixing db config and removing references to dev db

--- approach.rb
+++ approach.rb
@@ -1,6 +1,6 @@
 class Approach
   include Mongoid::Document
-  store_in collection: "approach", database: "ghc13"
+  store_in collection: "approach"
   field :idea  
   field :votes, type: Integer, default: 0
 end
